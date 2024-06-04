359c930f3f08a4ddc6a46ae3b200ee3300bdc070
QliQ.dev <abbas.fatullaev@gmail.com>
2024-06-04 22:19:41 +0600

    reproduction

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,66 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonFooter,
+  IonButton,
+  IonSpinner
+} from '@ionic/angular/standalone';
+import { LunaSDK } from 'capacitor-luna-id-sdk';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFooter, IonButton, IonSpinner, NgIf],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  init: boolean = false;
+  bestShot: SafeResourceUrl | null = null;
+
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  async ngOnInit() {
+    const {initialized} = await LunaSDK.initialize({
+      livenessEnabled: true,
+      interactionEnabled: true,
+      trackFaceIdentity: false,
+      interactionTimeout: 30000,
+      headers:{
+        'X-Authorization': 'Y2I3MDk2MWQtYTBlMi00MzY4LWFhNzgtOGUyYmI5N2M3NWExOjZDNTNFMTdDODVGNzk0RTg3RThGMEJCNDZENjU2Qjk2'
+      }
+    })
+    this.init = initialized;
+  }
+
+  async start() {
+    const { base64String } = await LunaSDK.start();
+    if (base64String) {
+      try {
+        const blob = this.b64toBlob(base64String);
+        const objectUrl = window.URL.createObjectURL(blob);
+        this.bestShot = this.sanitizer.bypassSecurityTrustResourceUrl(objectUrl);
+      } catch (e) {
+        console.error('BESTSHOT', e);
+      }
+    }
+  }
+
+  b64toBlob(
+    data: string,
+    contentType: string = ''
+  ) {
+    const binaryData = atob(data);
+    const bytes = new Uint8Array(binaryData.length);
+    for (let i = 0; i < binaryData.length; i++) {
+      bytes[i] = binaryData.charCodeAt(i);
+    }
+    return new Blob([bytes], { type: contentType });
+  }
 }
